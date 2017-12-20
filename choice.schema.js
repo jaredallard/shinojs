99b3d1389e44da009b1edac71a8f5460c335febb
Jared Allard <jaredallard@outlook.com>
2017-12-20 13:53:39 -0800

    chore: cleanup code structure, reduce code complexity

--- choice.schema.js
+++ choice.schema.js
@@ -9,28 +9,28 @@
 [
   {
     version: 2,       // control the version used to parse this brick
-    address: "hello", // allow referencing to this method outside of it's scope
+    address: 'hello', // allow referencing to this method outside of it's scope
     classifiers: [    // array of text classifiers
-      "hi",
-      "hello",
-      "whatever"
+      'hi',
+      'hello',
+      'whatever'
     ],
-    classifier: "yes", // or an existing classifier (system builtins)
-    call: "hello",     // or link to another brick
+    classifier: 'yes', // or an existing classifier (system builtins)
+    call: 'hello',     // or link to another brick
     default: [         // string, one of these options, default: root
-      "root",          // default to root level commands -> system, if children aren't met
-      "unknown",       // default to "command unknown" handler
-      "retry"          // retry, don't drop context 
+      'root',          // default to root level commands -> system, if children aren't met
+      'unknown',       // default to 'command unknown' handler
+      'retry'          // retry, don't drop context
     ],
-    action: "",        // method to call when conditions are met, return true / false to allow children prcoessing if available
-    children: [        // an array of "chilren" options, allowing you to track actions available *after* this action.
+    action: '',        // method to call when conditions are met, return true / false to allow children prcoessing if available
+    children: [        // an array of 'chilren' options, allowing you to track actions available *after* this action.
       {
         version: 2,    // copy of above object basically
-        address: "world", // evaluates to hello.world
+        address: 'world', // evaluates to hello.world
       },
       {
         version: 2,
-        call: "hello"  // link to another option
+        call: 'hello'  // link to another option
       }
     ]
   }
